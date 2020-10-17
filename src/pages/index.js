682aa1e788b4f54aecef83d39455c83119eaba94
Brandon <brandongines@gmail.com>
2020-10-17 15:25:01 -0400

    fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
-import ReactDOM from "react-dom"
 import React, { useEffect } from "react"
-import { Route, Redirect, useRouteMatch } from "react-router-dom"
 
 // Sections
 // import Blog from "./Blog"
-import About from "../components/About"
+
 import Layout from "../components/layout"
 import Intro from "../components/Intro"
-import Header from "../components/Header"
+
 // import Resume from "./Resume"
 // import Service from "./Service"
 // import Contact from "./Contact"
@@ -15,10 +13,7 @@ import Header from "../components/Header"
 // import Testimonial from "./Testimonial"
 // import BlogSinglePost from "./BlogSinglePost"
 // Components
-import { fadeIn } from "react-animations"
-import Helmet from "../components/common/Helmet"
 
-import RedirectAs404 from "../components/common/RedirectAs404"
 import styled, { keyframes } from "styled-components"
 import { fadeInRight } from "react-animations"
 
